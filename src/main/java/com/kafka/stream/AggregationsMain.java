cf8bcf8f549757ba8d2705de00fcf1dafa8cfeb4
Sandip Patil <sanppatil@gmail.com>
2020-05-25 15:06:11 -0400

    Added project

--- src/main/java/com/kafka/stream/AggregationsMain.java
+++ src/main/java/com/kafka/stream/AggregationsMain.java
@@ -8,14 +8,18 @@ import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.Topology;
+import org.apache.kafka.streams.kstream.KGroupedStream;
 import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.KTable;
+import org.apache.kafka.streams.kstream.Materialized;
+import org.apache.kafka.streams.kstream.Produced;
 
-public class StreamsMain {
+public class AggregationsMain {
 
 	public static void main(String[] args) {
 		// Set up the configuration.
 		final Properties props = new Properties();
-		props.put(StreamsConfig.APPLICATION_ID_CONFIG, "streams-example");
+		props.put(StreamsConfig.APPLICATION_ID_CONFIG, "aggregations-example");
 		props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
 		props.put(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, 0);
 		// Since the input topic uses Strings for both key and value, set the default
@@ -25,9 +29,27 @@ public class StreamsMain {
 
 		// Get the source stream.
 		final StreamsBuilder builder = new StreamsBuilder();
-		final KStream<String, String> source = builder.stream("streams-input-topic");
+		KStream<String, String> source = builder.stream("aggregations-input-topic");
 
-		source.to("streams-output-topic");
+		// Group the source stream by the existing Key.
+		KGroupedStream<String, String> groupedStream = source.groupByKey();
+
+		// Create an aggregation that totals the length in characters of the value for
+		// all records sharing the same key.
+		KTable<String, Integer> aggregatedTable = groupedStream.aggregate(() -> 0,
+				(aggKey, newValue, aggValue) -> aggValue + newValue.length(),
+				Materialized.with(Serdes.String(), Serdes.Integer()));
+		aggregatedTable.toStream().to("aggregations-output-charactercount-topic",
+				Produced.with(Serdes.String(), Serdes.Integer()));
+
+		// Count the number of records for each key.
+		KTable<String, Long> countedTable = groupedStream.count(Materialized.with(Serdes.String(), Serdes.Long()));
+		countedTable.toStream().to("aggregations-output-count-topic", Produced.with(Serdes.String(), Serdes.Long()));
+
+		// Combine the values of all records with the same key into a string separated
+		// by spaces.
+		KTable<String, String> reducedTable = groupedStream.reduce((aggValue, newValue) -> aggValue + " " + newValue);
+		reducedTable.toStream().to("aggregations-output-reduce-topic");
 
 		final Topology topology = builder.build();
 		final KafkaStreams streams = new KafkaStreams(topology, props);
@@ -37,7 +59,7 @@ public class StreamsMain {
 
 		// Attach a shutdown handler to catch control-c and terminate the application
 		// gracefully.
-		Runtime.getRuntime().addShutdownHook(new Thread("shutdown-hook") {
+		Runtime.getRuntime().addShutdownHook(new Thread("streams-shutdown-hook") {
 			@Override
 			public void run() {
 				streams.close();
